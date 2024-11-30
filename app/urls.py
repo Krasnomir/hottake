d4070a1029aeb4a3723e6bd23d717e80caa4816b
Krasnomir <130172058+Krasnomir@users.noreply.github.com>
2024-11-30 19:34:50 +0100

    Added basic user authentication system

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("home", views.home, name="home"),
-    path("login", views.login, name="login"),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login_view, name='login_view'), 
+    path('register/', views.register_view, name='register_view'),
 ]
